44d4e65580a75bbb3ad210a44e281e9be26a9fb7
BrendaBii <bii.brenda.bcb@gmail.com>
2023-06-27 18:34:00 +0300

    Completed assignment.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(userName, userEmail){
+    const formData = {
+        name: userName,
+        email: userEmail,
+    };
+
+    const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(res => res.json())
+    .then(obj => {
+        const body = document.querySelector("body");
+        const p = document.createElement("p");
+        p.innerHTML = obj.id;
+        body.appendChild(p);
+        console.log(obj)
+    })
+    .catch(function (error) {
+        const body = document.querySelector("body");
+        const p = document.createElement("p");
+        p.innerHTML = error.message;
+        body.appendChild(p);
+    });
+}
+submitData("Brenda", "brenda@gmail.com")
